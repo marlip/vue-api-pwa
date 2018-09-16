adc0ae3223b5bc33a10a337bdbed3e778aba0114
Marc Lipscke <m.lipscke@markenstuermer.de>
2018-09-16 13:39:53 +0200

    initial layout

--- src/main.js
+++ src/main.js
@@ -4,12 +4,14 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vuetify from 'vuetify'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
+Vue.use(Vuetify)
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
---
+++ src/views/Projects.vue
@@ -0,0 +1,6 @@
+<template>
+  <v-container fluid>
+    <h1>Projekte</h1>
+  </v-container>
+
+</template>
