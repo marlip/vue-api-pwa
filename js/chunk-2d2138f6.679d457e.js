4c01c8943d494e00fe7b4eea631e43c13bced398
Marc Lipscke <m.lipscke@markenstuermer.de>
2018-09-20 23:13:20 +0200

    deploy

---
+++ js/chunk-2d2138f6.679d457e.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d2138f6"],{acca:function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("v-container",{attrs:{fluid:""}},[t("h1",[e._v("Projekte")])])},o=[],r=t("2877"),u={},a=Object(r["a"])(u,c,o,!1,null,null,null);a.options.__file="Projects.vue";n["default"]=a.exports}}]);
+//# sourceMappingURL=chunk-2d2138f6.679d457e.js.map
---
+++ precache-manifest.56248be3b44ba10a8c12f6ca7af5fa58.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-api-pwa/robots.txt"
+  },
+  {
+    "revision": "e37cc6617d133fdc93cb",
+    "url": "/vue-api-pwa/js/chunk-vendors.740f953f.js"
+  },
+  {
+    "revision": "ef56acad52cd3c3f2f51",
+    "url": "/vue-api-pwa/js/chunk-745be817.7ccdd6e7.js"
+  },
+  {
+    "revision": "9db1ea62fec725844abf",
+    "url": "/vue-api-pwa/js/chunk-2d2138f6.679d457e.js"
+  },
+  {
+    "revision": "727e485f7b20b478f9fa",
+    "url": "/vue-api-pwa/js/app.79a08538.js"
+  },
+  {
+    "revision": "a5408f6f22dadd6f352f4e7d32ad506b",
+    "url": "/vue-api-pwa/index.html"
+  },
+  {
+    "revision": "e37cc6617d133fdc93cb",
+    "url": "/vue-api-pwa/css/chunk-vendors.0c3e5376.css"
+  },
+  {
+    "revision": "ef56acad52cd3c3f2f51",
+    "url": "/vue-api-pwa/css/chunk-745be817.0d04185b.css"
+  },
+  {
+    "revision": "727e485f7b20b478f9fa",
+    "url": "/vue-api-pwa/css/app.8508c032.css"
+  }
+];
